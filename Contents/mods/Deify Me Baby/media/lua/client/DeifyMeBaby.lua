25721b0572225f0a8476cfed2d6f81cc4f5caada
Burryaga <48227516+Burryaga@users.noreply.github.com>
2024-06-11 16:15:44 -0400

    Fixing MP issue where player cannot be God so soon.

--- Contents/mods/Deify Me Baby/media/lua/client/DeifyMeBaby.lua
+++ Contents/mods/Deify Me Baby/media/lua/client/DeifyMeBaby.lua
@@ -1,10 +1,19 @@
 -- By Burryaga, if you are here, go spam him with comments about how amazing his mod is, he'll be happy :D
+-- UPDATE: I, Burryaga, have arrived, and I am in fact NOT happy. This was supposed to be tested before upload! Use debug, kids!;)
 local AutomaticGod = {}
 
-AutomaticGod.deifyMeBaby = function(playerIndex, player)
-    player:setGodMod(true)
+AutomaticGod.deifyMeBaby = function(player)
+    local playerIndex = player:getPlayerNum()
+    local deified = AutomaticGod.deified
+    if not deified then 
+        AutomaticGod.deified = {} 
+    else
+        if deified[playerIndex] then return end
+        player:setGodMod(true)
+        deified[playerIndex] = true
+    end
 end
 
-Events.OnCreatePlayer.Add(AutomaticGod.deifyMeBaby)
+Events.OnPlayerUpdate.Add(AutomaticGod.deifyMeBaby)
 
 return AutomaticGod
